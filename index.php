24ad1e0cd5ca1c794cfa3fef47619beb9580a5bc
sato <sato990519@gmail.com>
2019-10-31 20:41:38 +0000

    input patterns

--- index.php
+++ index.php
@@ -11,7 +11,9 @@
         <div class="col s12">
           Price Paid:
           <div class="input-field inline">
-            <input placeholder="£X.XX" id="price_inline" type="text" class="validate"maxlength="8">
+            <input placeholder="£X.XX" id="price_inline" 
+            type="text" class="validate"maxlength="8"
+            pattern="[0-9£.{6,25}">
             </div>
           </div>
   <button class="btn grey darken-2">Generate Invoice</button>
