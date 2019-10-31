fd079d45638f0133f2ac7df4c592f63429b58c9e
sato <sato990519@gmail.com>
2019-10-31 20:16:21 +0000

    dddd

--- index.php
+++ index.php
@@ -7,7 +7,17 @@
   
   <div align="center">
 <form>
-Price Paid: <input type="text" size="6" maxlength="6"> <br>
+<div class="row">
+        <div class="col s12">
+          Price Paid:
+          <div class="input-field inline">
+            <input id="email_inline" type="email" class="validate">
+            <label for="#">Email</label>
+            <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
+          </div>
+        </div>
+      </div>
+      <br>
   <button type="submit" name="checkin-submit" class="btn grey darken-2">Generate Invoice</button>
   </form>
 <br>
