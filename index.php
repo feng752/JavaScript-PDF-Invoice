4fe8eb8af4ec0c3dd503b48ab7449622ac188955
sato <sato990519@gmail.com>
2019-10-31 19:55:06 +0000

    button

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
 <main>
   
   <div align="center"class="topContent">
+  <button type="submit" name="checkin-submit" class="btn grey darken-2">Check In!</button>
    <?php 
    echo "Current Date: $formatDate<br>"; 
    ?>
   </div>
-  <button type="submit" name="checkin-submit" class="btn grey darken-2">Check In!</button>
 </div>
 </div>
 </form>
