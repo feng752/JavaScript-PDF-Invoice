a528e6c98ce386934fd222abc220912032240734
sato <sato990519@gmail.com>
2019-11-03 19:11:01 +0000

    invalid date?

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
    // select the input
    let input = document.querySelector('input');
 
-   var today = new Date("d F Y, h:i:s A");
+   var today = new Date("d F Y, h:i:s");
    var newdat= today;
   // Listen for the users click
    button.addEventListener('click', printPDF)
